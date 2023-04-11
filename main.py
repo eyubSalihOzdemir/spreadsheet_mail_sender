f3a15bee8e7149a3417c04acfb32d0af4c8e3607
eyubSalihOzdemir <eyupsalih776@gmail.com>
2023-04-11 05:18:59 +0300

    file structure

--- main.py
+++ main.py
@@ -19,11 +19,3 @@ if __name__ == '__main__':
     # Write data to spreadsheet, because 'Mail Sent' field might have changed
     data = get_data_from_recipients(recipients)
     sheet.write('Sheet1!A2:F', data)
-
-
-# 1. Create a project on Google Cloud console
-# 2. Create a service account
-# 3. Create a JSON key for that service account and save it locally (edit path in emailer/constants.py)
-# 4. Share your Google Sheet with service account email address
-# 5. Extract sheet id from your sheets URL and save it locally (edit in emailer/constants.py)
-# 6. From Google Account/Security settings, create a 16-digit App Password for your email and save it locally (edit in emailer/constants.py)
